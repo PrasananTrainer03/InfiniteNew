d46129c1f7029f60d6a5fccc088791d18677ae8f
Prasanna <prassup.dotnet@gmail.com>
2021-11-02 16:07:34 +0530

    code

---
+++ Day39_Spring_Imp/SpringDemo3/src/main/java/com/java/spr/MainProg.java
@@ -0,0 +1,19 @@
+package com.java.spr;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainProg {
+
+	public static void main(String[] args) {
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("com/java/spr/person.xml");
+		Person p1 = (Person)ctx.getBean("beanAishwarya");
+		p1.showAllInfo();
+		
+		Person p2 = (Person)ctx.getBean("beanRaghuvir");
+		p2.showAllInfo();
+		
+		Person p3 = (Person)ctx.getBean("beanTejaswini");
+		p3.showAllInfo();
+	}
+}
