772590b9a52c2ca46b98c24b462f4eecc59dd2ca
Prasanna <prassup.dotnet@gmail.com>
2021-11-17 11:21:29 +0530

    code

---
+++ Ejb Demos/EjbHelloWorld/ejbModule/com/java/ejb/HelloWorldBean.java
@@ -0,0 +1,27 @@
+package com.java.ejb;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class HelloWorldBean
+ */
+@Stateless
+@Remote(HelloWorldBeanRemote.class)
+public class HelloWorldBean implements HelloWorldBeanRemote {
+
+    /**
+     * Default constructor. 
+     */
+    public HelloWorldBean() {
+        // TODO Auto-generated constructor stub
+    }
+
+	@Override
+	public String sayHello() {
+		// TODO Auto-generated method stub
+		return "Welcome to EJB Programmig...From Prasanna";
+	}
+
+}
